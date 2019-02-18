214cbaaebec1955e67ba0f05e6da1aab43b5edd3
yoursock <ksx74@qq.com>
2019-02-18 17:25:04 +0800

    redis版本不同引发的一些错误，需继续修改

--- proxypool/crawler.py
+++ proxypool/crawler.py
@@ -23,7 +23,7 @@ class Crawler(object, metaclass=ProxyMetaclass):
             print('成功获取到代理', proxy)
             proxies.append(proxy)
         return proxies
-       
+
     def crawl_daili66(self, page_count=4):
         """
         获取代理66
@@ -51,45 +51,45 @@ class Crawler(object, metaclass=ProxyMetaclass):
             # \s * 匹配空格，起到换行作用
             re_ip_address = ip_address.findall(html)
             for address, port in re_ip_address:
-                result = address+':'+ port
+                result = address + ':' + port
                 yield result.replace(' ', '')
-    
+
     def crawl_kuaidaili(self):
         for i in range(1, 4):
             start_url = 'http://www.kuaidaili.com/free/inha/{}/'.format(i)
             html = get_page(start_url)
             if html:
-                ip_address = re.compile('<td data-title="IP">(.*?)</td>') 
+                ip_address = re.compile('<td data-title="IP">(.*?)</td>')
                 re_ip_address = ip_address.findall(html)
                 port = re.compile('<td data-title="PORT">(.*?)</td>')
                 re_port = port.findall(html)
-                for address,port in zip(re_ip_address, re_port):
-                    address_port = address+':'+port
-                    yield address_port.replace(' ','')
+                for address, port in zip(re_ip_address, re_port):
+                    address_port = address + ':' + port
+                    yield address_port.replace(' ', '')
 
     def crawl_xicidaili(self):
         for i in range(1, 3):
             start_url = 'http://www.xicidaili.com/nn/{}'.format(i)
             headers = {
-                'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                'Cookie':'_free_proxy_session=BAh7B0kiD3Nlc3Npb25faWQGOgZFVEkiJWRjYzc5MmM1MTBiMDMzYTUzNTZjNzA4NjBhNWRjZjliBjsAVEkiEF9jc3JmX3Rva2VuBjsARkkiMUp6S2tXT3g5a0FCT01ndzlmWWZqRVJNek1WanRuUDBCbTJUN21GMTBKd3M9BjsARg%3D%3D--2a69429cb2115c6a0cc9a86e0ebe2800c0d471b3',
-                'Host':'www.xicidaili.com',
-                'Referer':'http://www.xicidaili.com/nn/3',
-                'Upgrade-Insecure-Requests':'1',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                'Cookie': '_free_proxy_session=BAh7B0kiD3Nlc3Npb25faWQGOgZFVEkiJWRjYzc5MmM1MTBiMDMzYTUzNTZjNzA4NjBhNWRjZjliBjsAVEkiEF9jc3JmX3Rva2VuBjsARkkiMUp6S2tXT3g5a0FCT01ndzlmWWZqRVJNek1WanRuUDBCbTJUN21GMTBKd3M9BjsARg%3D%3D--2a69429cb2115c6a0cc9a86e0ebe2800c0d471b3',
+                'Host': 'www.xicidaili.com',
+                'Referer': 'http://www.xicidaili.com/nn/3',
+                'Upgrade-Insecure-Requests': '1',
             }
             html = get_page(start_url, options=headers)
             if html:
                 find_trs = re.compile('<tr class.*?>(.*?)</tr>', re.S)
                 trs = find_trs.findall(html)
                 for tr in trs:
-                    find_ip = re.compile('<td>(\d+\.\d+\.\d+\.\d+)</td>') 
+                    find_ip = re.compile('<td>(\d+\.\d+\.\d+\.\d+)</td>')
                     re_ip_address = find_ip.findall(tr)
                     find_port = re.compile('<td>(\d+)</td>')
                     re_port = find_port.findall(tr)
-                    for address,port in zip(re_ip_address, re_port):
-                        address_port = address+':'+port
-                        yield address_port.replace(' ','')
-    
+                    for address, port in zip(re_ip_address, re_port):
+                        address_port = address + ':' + port
+                        yield address_port.replace(' ', '')
+
     def crawl_ip3366(self):
         for i in range(1, 4):
             start_url = 'http://www.ip3366.net/?stype=1&page={}'.format(i)
@@ -102,10 +102,10 @@ class Crawler(object, metaclass=ProxyMetaclass):
                     re_ip_address = find_ip.findall(trs[s])
                     find_port = re.compile('<td>(\d+)</td>')
                     re_port = find_port.findall(trs[s])
-                    for address,port in zip(re_ip_address, re_port):
-                        address_port = address+':'+port
-                        yield address_port.replace(' ','')
-    
+                    for address, port in zip(re_ip_address, re_port):
+                        address_port = address + ':' + port
+                        yield address_port.replace(' ', '')
+
     def crawl_iphai(self):
         start_url = 'http://www.iphai.com/'
         html = get_page(start_url)
@@ -117,9 +117,9 @@ class Crawler(object, metaclass=ProxyMetaclass):
                 re_ip_address = find_ip.findall(trs[s])
                 find_port = re.compile('<td>\s+(\d+)\s+</td>', re.S)
                 re_port = find_port.findall(trs[s])
-                for address,port in zip(re_ip_address, re_port):
-                    address_port = address+':'+port
-                    yield address_port.replace(' ','')
+                for address, port in zip(re_ip_address, re_port):
+                    address_port = address + ':' + port
+                    yield address_port.replace(' ', '')
 
     def crawl_data5u(self):
         start_url = 'http://www.data5u.com/free/gngn/index.shtml'
@@ -142,6 +142,3 @@ class Crawler(object, metaclass=ProxyMetaclass):
             for address, port in re_ip_address:
                 result = address + ':' + port
                 yield result.replace(' ', '')
-
-
-            
--- proxypool/db.py
+++ proxypool/db.py
@@ -15,7 +15,7 @@ class RedisClient(object):
         :param password: Redis密码
         """
         self.db = redis.StrictRedis(host=host, port=port, password=password, decode_responses=True)
-    
+
     def add(self, proxy, score=INITIAL_SCORE):
         """
         添加代理，设置分数为最高
@@ -27,14 +27,16 @@ class RedisClient(object):
             print('代理不符合规范', proxy, '丢弃')
             return
         if not self.db.zscore(REDIS_KEY, proxy):
-            return self.db.zadd(REDIS_KEY, score, proxy)
-    
+            # return self.db.zadd(REDIS_KEY, score, proxy)
+            return self.db.zadd(REDIS_KEY, {proxy: score}, proxy)  # redis版本更新后，zadd方法的第二个参数是字典
+
     def random(self):
         """
         随机获取有效代理，首先尝试获取最高分数代理，如果不存在，按照排名获取，否则异常
         :return: 随机代理
         """
         result = self.db.zrangebyscore(REDIS_KEY, MAX_SCORE, MAX_SCORE)
+        # result = self.db.zrangebyscore(REDIS_KEY, MIN_SCORE, MAX_SCORE)
         if len(result):
             return choice(result)
         else:
@@ -43,7 +45,7 @@ class RedisClient(object):
                 return choice(result)
             else:
                 raise PoolEmptyError
-    
+
     def decrease(self, proxy):
         """
         代理值减一分，小于最小值则删除
@@ -54,10 +56,11 @@ class RedisClient(object):
         if score and score > MIN_SCORE:
             print('代理', proxy, '当前分数', score, '减1')
             return self.db.zincrby(REDIS_KEY, proxy, -1)
+            # return self.db.zincrby(REDIS_KEY, {proxy: score}, -1)
         else:
             print('代理', proxy, '当前分数', score, '移除')
             return self.db.zrem(REDIS_KEY, proxy)
-    
+
     def exists(self, proxy):
         """
         判断是否存在
@@ -65,7 +68,7 @@ class RedisClient(object):
         :return: 是否存在
         """
         return not self.db.zscore(REDIS_KEY, proxy) == None
-    
+
     def max(self, proxy):
         """
         将代理设置为MAX_SCORE
@@ -73,22 +76,23 @@ class RedisClient(object):
         :return: 设置结果
         """
         print('代理', proxy, '可用，设置为', MAX_SCORE)
-        return self.db.zadd(REDIS_KEY, MAX_SCORE, proxy)
-    
+        # return self.db.zadd(REDIS_KEY, MAX_SCORE, proxy)
+        return self.db.zadd(REDIS_KEY, {proxy: MAX_SCORE}, proxy)
+
     def count(self):
         """
         获取数量
         :return: 数量
         """
         return self.db.zcard(REDIS_KEY)
-    
+
     def all(self):
         """
         获取全部代理
         :return: 全部代理列表
         """
         return self.db.zrangebyscore(REDIS_KEY, MIN_SCORE, MAX_SCORE)
-    
+
     def batch(self, start, stop):
         """
         批量获取
